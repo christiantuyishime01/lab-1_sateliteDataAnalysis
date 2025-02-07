eeeeecbbd56e7fa401f76a553e633558ce7803f2
ksanthiana <s.kaze@alustudent.com>
2025-02-07 13:50:51 -0800

    I have created bash for Burundi humidity data

--- raw_data/humidity_data_Burundi.sh
+++ raw_data/humidity_data_Burundi.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-grep "Burundi" satelite_temperature_data.csv | sort -t',' -k4 -nr > analyzed_data/humidity_data_Burundi.csv
+echo "Extracting humidity data for $COUNTRY..."
+grep "$BURUNDI" raw_data/satelite_temperature_data.csv | sort -t',' -k4 -nr > analyzed_data/humidity_data_${BURUNDI}.csv
+
+echo "Analysis complete! Check the analyzed_data directory for results."
