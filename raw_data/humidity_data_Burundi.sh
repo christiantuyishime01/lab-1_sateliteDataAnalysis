36148636cd5e72911860c15d44aa6aaead709e18
ksanthiana <s.kaze@alustudent.com>
2025-02-07 04:20:42 -0800

    I have created a bash for Burundi humidity data

--- raw_data/humidity_data_Burundi.sh
+++ raw_data/humidity_data_Burundi.sh
@@ -3,4 +3,4 @@
 input="/c/Users/ainez/lab-1_sateliteDataAnalysis/raw_data/satelite_temperature_data.csv"
 output="/c/Users/ainez/lab-1_sateliteDataAnalysis/analyzed_data/humidity_data_Burundi.csv"
 
-grep 'Burundi' "$input" | sort -t',' -k4,4nr > "$output"
+grep "Burundi" "$input" | sort -t',' -k4,4 -nr > "$output"
