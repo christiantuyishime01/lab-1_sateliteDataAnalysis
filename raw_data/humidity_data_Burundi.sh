628abe5d03fb01178428982aff16b64fec39a6a3
ksanthiana <s.kaze@alustudent.com>
2025-02-08 12:17:08 -0800

    I have created bash for Burundi humidity data

--- raw_data/humidity_data_Burundi.sh
+++ raw_data/humidity_data_Burundi.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 input="/c/Users/ainez/lab-1_sateliteDataAnalysis/raw_data/satelite_temperature_data.csv"
 output="/c/Users/ainez/lab-1_sateliteDataAnalysis/analyzed_data/humidity_data_Burundi.csv"
-grep "$BURUNDI" "input" | sort -t',' -k4 -nr > "output"
+country="Burundi"
+grep "$BURUNDI" "$input" | sort -t, -k4,4nr > "$output"
+echo "Humidity data for $Burundi saved to $output"
