0c07f236a9d5b412fddb97b9a60f6d958e7c3119
Manziine <a.manziine@alustudent.com>
2025-02-09 18:51:23 +0200

    bash for Burundi humidity data

--- raw_data/humidity_data_Burundi.sh
+++ raw_data/humidity_data_Burundi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
-input="/c/Users/ainez/lab-1_sateliteDataAnalysis/raw_data/satelite_temperature_data.csv"
-output="/c/Users/ainez/lab-1_sateliteDataAnalysis/analyzed_data/humidity_data_Burundi.csv"
+input="satelite_temperature_data.csv"
+output="../analyzed_data/humidity_data_Burundi.csv"
 
 
 grep 'Burundi' "$input" | cut -d',' -f1,2,3 | sort -t',' -k3,3nr > "$output"
